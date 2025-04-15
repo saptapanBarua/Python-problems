7e90d363e69bbdcd12a66513e8fffa193baaa140
saptapanBarua <sapta.barua@gmail.com>
2025-04-15 06:09:28 -0700

    prob6 done with some errors

--- prob6.py
+++ prob6.py
@@ -1,1 +1,13 @@
-print('Hello')
+s1 = "Phi"
+s2 = "Phitron"
+
+isMatched = False
+
+for i in range(len(s1)):
+	if s1[i] == s2[i]:
+		isMatched = True
+
+print('Yes')if isMatched else print('No')
+
+
+# there is a error.
